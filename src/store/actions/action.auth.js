c75e5332bfa889145a4e7818ff3f9a84f9a046ca
alexbarksdale <ale.barksdale@gmail.com>
2020-02-15 19:51:12 -0800

    [Add] Redux actions

--- src/store/actions/action.auth.js
+++ src/store/actions/action.auth.js
@@ -1,1 +1,35 @@
+import buddiup from '../../apis/buddiup';
 import { AUTH_START, AUTH_SUCCESS, AUTH_FAIL, AUTH_LOGOUT } from './action.types';
+
+export const authStart = () => {
+    return { type: AUTH_START };
+};
+
+export const authSucces = (token) => {
+    return { type: AUTH_SUCCESS, payload: token };
+};
+
+export const authFail = (error) => {
+    return { type: AUTH_FAIL, payload: error };
+};
+
+export const authLogout = () => {
+    return { type: AUTH_LOGOUT };
+};
+
+export const authSignUp = (userData) => (dispatch) => {
+    dispatch(authStart());
+    buddiup
+        .post('/api/auth/register', userData)
+        .then((res) => {
+            console.log(res);
+        })
+        .catch((err) => {
+            dispatch(authFail(err));
+            console.log(err);
+        });
+};
+
+// export const authLogin = (userData) => (dispatch) => {
+//     buddiup.post('/api/auth/login', userData).then((res) => {});
+// };
